4542e5b9074dc137fe860b8f3f483f718b8522bd
q82419 <q82419@gmail.com>
2017-04-12 19:54:09 +0800

    fix map

--- js/request.js
+++ js/request.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
                 var seat_state = parseInt(data['ticket'][i]['state']);
                 var seat_type = parseInt(data['ticket'][i]['type']);
                 var seat_preserve = parseInt(data['ticket'][i]['preserve']);
-                if(seat_preserve == 1 || seat_preserve == 4 || seat_preserve == 5)
+                if(seat_preserve > 0)
                     $(seat_id).addClass('seat_type_6');
                 else if(seat_state == 2)
                     $(seat_id).addClass('seat_type_5');
